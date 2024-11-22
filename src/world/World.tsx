981c4f52c59e9eebc65dd50185619d3b9a8e5dd5
John M <entity.john@gmail.com>
2024-11-21 21:46:48 -0600

    cool snackbar idea

--- src/world/World.tsx
+++ src/world/World.tsx
@@ -8,6 +8,7 @@ import MapDisplay3D from "./MapDisplay3D"
 import GridHelper from "./GridHelper"
 import { CAMERA_FOV } from "../utils/constants"
 import ForestTree10WithBase from "./maphex/ForestTree10WithBase"
+import { Alert, Button, Snackbar } from "@mui/material"
 
 const World = () => {
     const cameraControlsRef = React.useRef(undefined!)
@@ -35,6 +36,27 @@ const World = () => {
                 <GridHelper />
                 <MyCameraControls cameraControlsRef={cameraControlsRef} />
             </Canvas>
+            <Snackbar
+                open={true}
+                anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+                autoHideDuration={2000}
+                message="This Snackbar will be dismissed in 5 seconds."
+                action={
+                    <Button color="inherit" size="small">
+                        Undo
+                    </Button>
+                }
+                sx={{ bottom: { xl: 320, lg: 300, md: 300, sm: 20, xs: 20 } }}
+            >
+                <Alert
+                    // onClose={handleClose}
+                    severity="success"
+                    variant="filled"
+                    sx={{ width: '100%' }}
+                >
+                    This is a success Alert inside a Snackbar!
+                </Alert>
+            </Snackbar>
         </div>
     )
 }
