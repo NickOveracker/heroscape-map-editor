933dea0fbce72dcb5144b73792cefa5f1e30e813
John M <entity.john@gmail.com>
2024-12-20 00:23:19 -0600

    tidy up notistack demo

--- src/controls/Controls.tsx
+++ src/controls/Controls.tsx
@@ -1,4 +1,4 @@
-import { Button, Container } from '@mui/material'
+import { Container } from '@mui/material'
 import DEVLogSomethingCoolButton from './DEVLogSomethingCoolButton'
 import FileButtonGroup from './FileButtonGroup'
 // import MapLensToggles from './MapLensToggles'
@@ -7,19 +7,8 @@ import PenTerrainSelect from './PenTerrainSelect'
 import TakePictureButtonGroup from './TakePictureButtonGroup'
 import ToggleCameraButton from './ToggleCameraButton'
 import RotationSelect from './RotationSelect'
-import { useSnackbar, VariantType } from 'notistack'
 
 const Controls = () => {
-    const { enqueueSnackbar } = useSnackbar();
-
-    const handleClick = () => {
-        enqueueSnackbar('I love snacks.');
-    };
-
-    const handleClickVariant = (variant: VariantType) => () => {
-        // variant could be success, error, warning, info, or default
-        enqueueSnackbar('This is a success message!', { variant });
-    };
     return (
         <Container sx={{ padding: 1 }}>
             <PenTerrainSelect />
@@ -30,8 +19,6 @@ const Controls = () => {
             <TakePictureButtonGroup />
             <ToggleCameraButton />
             <DEVLogSomethingCoolButton />
-            <Button onClick={handleClick}>Show snackbar</Button>
-            <Button onClick={handleClickVariant('success')}>Show success snackbar</Button>
         </Container>
     )
 }
