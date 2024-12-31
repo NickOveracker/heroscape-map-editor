5d8f0ea92ba28466a300c25b1c39c2cc8e46b896
John M <entity.john@gmail.com>
2024-12-31 12:45:11 -0600

    add sentry error reporting
    dunno if its working yet

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { createTheme, CssBaseline, ThemeProvider } from '@mui/material'
 import { Route, Switch } from "wouter";
 import { SnackbarProvider } from 'notistack'
+import * as Sentry from "@sentry/react";
+
 import { EventProvider } from './hooks/useEvent'
 import ErrorPage from './layout/ErrorPage';
 import HomePage from './layout/HomePage';
@@ -14,23 +16,25 @@ const darkTheme = createTheme({
 
 const App = () => {
   return (
-    <ThemeProvider theme={darkTheme}>
-      <CssBaseline />
-      <EventProvider>
-        <SnackbarProvider
-          anchorOrigin={{ horizontal: 'center', vertical: 'top' }}
-          maxSnack={3}
-          autoHideDuration={3000}
-        >
-          <Switch>
-            <Route path="/" component={HomePage} />
+    <Sentry.ErrorBoundary fallback={ErrorPage} showDialog>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <EventProvider>
+          <SnackbarProvider
+            anchorOrigin={{ horizontal: 'center', vertical: 'top' }}
+            maxSnack={3}
+            autoHideDuration={3000}
+          >
+            <Switch>
+              <Route path="/" component={HomePage} />
 
-            {/* Default route in a switch */}
-            <Route><ErrorPage /></Route>
-          </Switch>
-        </SnackbarProvider>
-      </EventProvider>
-    </ThemeProvider>
+              {/* Default route in a switch */}
+              <Route><ErrorPage /></Route>
+            </Switch>
+          </SnackbarProvider>
+        </EventProvider>
+      </ThemeProvider>
+    </Sentry.ErrorBoundary>
   )
 }
 
---
+++ src/sentry.js
@@ -0,0 +1,24 @@
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://abf54ce42681460cbea61d773becd43d@o374574.ingest.us.sentry.io/5192792",
+  integrations: [
+    Sentry.replayIntegration(),
+  ],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for tracing.
+  // Learn more at
+  // https://docs.sentry.io/platforms/javascript/configuration/options/#traces-sample-rate
+  tracesSampleRate: 1.0,
+
+  // Set `tracePropagationTargets` to control for which URLs trace propagation should be enabled
+  tracePropagationTargets: [/^\//, /^https:\/\/yourserver\.io\/api/],
+
+  // Capture Replay for 10% of all sessions,
+  // plus for 100% of sessions with an error
+  // Learn more at
+  // https://docs.sentry.io/platforms/javascript/session-replay/configuration/#general-integration-configuration
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
