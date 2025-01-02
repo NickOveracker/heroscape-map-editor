5e67fe7f18f4628866ba62c4fd27d51e30a4e713
John M <entity.john@gmail.com>
2025-01-01 22:54:32 -0600

    trim Laur Pillar, for prod

--- src/world/models/LaurPillar.tsx
+++ src/world/models/LaurPillar.tsx
@@ -1,90 +1,85 @@
 import React from 'react'
 import { ThreeEvent } from '@react-three/fiber'
 import { DoubleSide } from 'three'
-import { Billboard, Html, useGLTF } from '@react-three/drei'
-import { BoardHex, BoardHexes, BoardPieces, HexTerrain, Pieces } from '../../types'
-import { decodePieceID, getBoardHex3DCoords, getHexNeighborByRotAlt, getHexNearNeighborByRotation, pillarSideRotations, genBoardHexID } from '../../utils/map-utils'
+import { useGLTF } from '@react-three/drei'
+import { BoardHex, HexTerrain } from '../../types'
+import { getBoardHex3DCoords } from '../../utils/map-utils'
 import ObstacleBase from './ObstacleBase'
 import { hexTerrainColor } from '../maphex/hexColors'
-import useBoundStore from '../../store/store'
-import { Button } from '@mui/material'
-import usePieceHoverState from '../../hooks/usePieceHoverState'
-import { hexUtilsAdd, hexUtilsGetNeighborForRotation, hexUtilsGetRadialNearNeighborsForRotation } from '../../utils/hex-utils'
-import { isFluidTerrainHex, isSolidTerrainHex } from '../../utils/board-utils'
 
 
 
-function getPillarReport({
-  boardHexes,
-  boardPieces,
-  boardHex
-}: {
-  boardHexes: BoardHexes
-  boardPieces: BoardPieces
-  boardHex: BoardHex
-}) {
-  const pillarAddons = Object.keys(boardPieces)
-    .map(key => decodePieceID(key))
-    .filter(piece => piece.pieceID === Pieces.laurWallLong ||
-      piece.pieceID === Pieces.laurWallShort ||
-      piece.pieceID === Pieces.laurWallRuin)
-  const sideLandHexes = pillarSideRotations.map(sideRot => {
-    const actualRotation = (boardHex.pieceRotation + sideRot) % 6
-    return getHexNeighborByRotAlt(boardHex, boardHexes, actualRotation, -1) // pass -1 to get one below pillar
-  })
-  const sidePillarHexes = pillarSideRotations.map(sideRot => {
-    const actualRotation = (boardHex.pieceRotation + sideRot) % 6
-    const pillarHex = getHexNeighborByRotAlt(boardHex, boardHexes, actualRotation)
-    if (pillarHex?.pieceID?.includes(Pieces.laurWallPillar) && pillarHex?.isObstacleOrigin) {
-      return pillarHex
-    } else {
-      return
-    }
-  })
-  const sideCanBuildRuins = pillarSideRotations.map(sideRot => {
-    const actualRotation = (boardHex.pieceRotation + sideRot) % 6
-    console.log("🚀 ~ actualRotation:", actualRotation)
-    const coordsObstructedByRuins = Number.isInteger(actualRotation)
-      ? [hexUtilsGetNeighborForRotation(actualRotation)]
-      : hexUtilsGetRadialNearNeighborsForRotation(actualRotation)
-    const piecePlaneCoords = coordsObstructedByRuins.filter(c => !!c).map((coord) =>
-      hexUtilsAdd(coord, { q: boardHex.q, r: boardHex.r, s: boardHex.s, }),
-    )
-    console.log("🚀 ~ piecePlaneCoords:", piecePlaneCoords)
-    const isVerticalClearanceForPiece = piecePlaneCoords.every((coord, i) => {
-      if (!coord) { return false }
-      const clearanceHexIds = Array(10) //using 10, not 9, because unlike in addPiece we are not starting from the placement altitude
-        .fill(0)
-        .map((_, j) => {
-          const altitude = boardHex.altitude + j
-          return genBoardHexID({ ...piecePlaneCoords[i], altitude })
-        })
-      return clearanceHexIds.every((clearanceHexId) => {
-        const hex = boardHexes?.[clearanceHexId]
-        if (!hex) return true // if no boardHex is written, then it is definitely empty
-        const terrain = hex.terrain
-        const isBlocked =
-          isSolidTerrainHex(terrain) ||
-          isFluidTerrainHex(terrain)
-        return !isBlocked
-      })
-    })
-    if (isVerticalClearanceForPiece) {
-      return true
-    } else {
-      return false
-    }
-  })
+// function getPillarReport({
+//   boardHexes,
+//   boardPieces,
+//   boardHex
+// }: {
+//   boardHexes: BoardHexes
+//   boardPieces: BoardPieces
+//   boardHex: BoardHex
+// }) {
+//   const pillarAddons = Object.keys(boardPieces)
+//     .map(key => decodePieceID(key))
+//     .filter(piece => piece.pieceID === Pieces.laurWallLong ||
+//       piece.pieceID === Pieces.laurWallShort ||
+//       piece.pieceID === Pieces.laurWallRuin)
+//   const sideLandHexes = pillarSideRotations.map(sideRot => {
+//     const actualRotation = (boardHex.pieceRotation + sideRot) % 6
+//     return getHexNeighborByRotAlt(boardHex, boardHexes, actualRotation, -1) // pass -1 to get one below pillar
+//   })
+//   const sidePillarHexes = pillarSideRotations.map(sideRot => {
+//     const actualRotation = (boardHex.pieceRotation + sideRot) % 6
+//     const pillarHex = getHexNeighborByRotAlt(boardHex, boardHexes, actualRotation)
+//     if (pillarHex?.pieceID?.includes(Pieces.laurWallPillar) && pillarHex?.isObstacleOrigin) {
+//       return pillarHex
+//     } else {
+//       return
+//     }
+//   })
+//   const sideCanBuildRuins = pillarSideRotations.map(sideRot => {
+//     const actualRotation = (boardHex.pieceRotation + sideRot) % 6
+//     console.log("🚀 ~ actualRotation:", actualRotation)
+//     const coordsObstructedByRuins = Number.isInteger(actualRotation)
+//       ? [hexUtilsGetNeighborForRotation(actualRotation)]
+//       : hexUtilsGetRadialNearNeighborsForRotation(actualRotation)
+//     const piecePlaneCoords = coordsObstructedByRuins.filter(c => !!c).map((coord) =>
+//       hexUtilsAdd(coord, { q: boardHex.q, r: boardHex.r, s: boardHex.s, }),
+//     )
+//     console.log("🚀 ~ piecePlaneCoords:", piecePlaneCoords)
+//     const isVerticalClearanceForPiece = piecePlaneCoords.every((coord, i) => {
+//       if (!coord) { return false }
+//       const clearanceHexIds = Array(10) //using 10, not 9, because unlike in addPiece we are not starting from the placement altitude
+//         .fill(0)
+//         .map((_, j) => {
+//           const altitude = boardHex.altitude + j
+//           return genBoardHexID({ ...piecePlaneCoords[i], altitude })
+//         })
+//       return clearanceHexIds.every((clearanceHexId) => {
+//         const hex = boardHexes?.[clearanceHexId]
+//         if (!hex) return true // if no boardHex is written, then it is definitely empty
+//         const terrain = hex.terrain
+//         const isBlocked =
+//           isSolidTerrainHex(terrain) ||
+//           isFluidTerrainHex(terrain)
+//         return !isBlocked
+//       })
+//     })
+//     if (isVerticalClearanceForPiece) {
+//       return true
+//     } else {
+//       return false
+//     }
+//   })
 
-  // console.log("🚀 ~ pillarAddons ~ pillarAddons:", pillarAddons)
-  // console.log("🚀 ~ sideLandHexes:", sideLandHexes)
-  // console.log("🚀 ~ sidePillarHexes:", sidePillarHexes)
-  // console.log("🚀 ~ sideCanBuildRuins:", sideCanBuildRuins)
-}
+//   // console.log("🚀 ~ pillarAddons ~ pillarAddons:", pillarAddons)
+//   // console.log("🚀 ~ sideLandHexes:", sideLandHexes)
+//   // console.log("🚀 ~ sidePillarHexes:", sidePillarHexes)
+//   // console.log("🚀 ~ sideCanBuildRuins:", sideCanBuildRuins)
+// }
 
 export default function LaurWallPillar({
   boardHex,
-  onPointerUpLaurWall,
+  // onPointerUpLaurWall,
 }: {
   boardHex: BoardHex
   onPointerUpLaurWall: (
@@ -93,91 +88,61 @@ export default function LaurWallPillar({
   ) => void
 }) {
   const { x, z, yBaseCap, yWithBase } = getBoardHex3DCoords(boardHex)
-  const selectedPieceID = useBoundStore(s => s.selectedPieceID)
-  const boardHexes = useBoundStore(s => s.boardHexes)
-  const boardPieces = useBoundStore(s => s.boardPieces)
+  // const selectedPieceID = useBoundStore(s => s.selectedPieceID)
+  // const boardHexes = useBoundStore(s => s.boardHexes)
+  // const boardPieces = useBoundStore(s => s.boardPieces)
   const { nodes } = useGLTF('/laurwall-pillar.glb') as any
   const rotation = boardHex?.pieceRotation ?? 0
-  const {
-    isHovered,
-    onPointerEnter,
-    onPointerOut,
-  } = usePieceHoverState()
+  // const {
+  //   isHovered,
+  //   onPointerEnter,
+  //   onPointerOut,
+  // } = usePieceHoverState()
   const pillarColor = hexTerrainColor[HexTerrain.laurWall]
   const interiorPillarColor = hexTerrainColor.laurModelColor2
-  const yellowColor = 'yellow'
-  const isSelected = selectedPieceID === boardHex.pieceID
-  const isHighlighted = isHovered || isSelected
+  // const yellowColor = 'yellow'
+  // const isSelected = selectedPieceID === boardHex.pieceID
+  // const isHighlighted = isHovered || isSelected
 
-  if (isSelected) {
-    const pillarReport = getPillarReport({
-      boardHexes,
-      boardPieces,
-      boardHex
-    })
-  }
+  // if (isSelected) {
+  //   const pillarReport = getPillarReport({
+  //     boardHexes,
+  //     boardPieces,
+  //     boardHex
+  //   })
+  // }
 
-  const buttonPositions: [x: number, y: number, z: number][] = [
-    [0.5, 1, 0],
-    [-0.5, 1, 1],
-    [-1.5, 1, 0],
-    [-0.5, 1, -1],
-  ].map(xyz => [xyz[0] - 0.1, xyz[1], xyz[2] - 0.2])
+  // const buttonPositions: [x: number, y: number, z: number][] = [
+  //   [0.5, 1, 0],
+  //   [-0.5, 1, 1],
+  //   [-1.5, 1, 0],
+  //   [-0.5, 1, -1],
+  // ].map(xyz => [xyz[0] - 0.1, xyz[1], xyz[2] - 0.2])
 
   return (
     <>
       <group
         position={[x, yWithBase, z]}
-      >
-        {/* {isSelected && pillarSideRotations.map } */}
-        {(isSelected) && (
-          <Billboard
-            position={buttonPositions[0]}>
-            <Html>
-              <Button variant='contained' size="small">1,0</Button>
-            </Html>
-          </Billboard>)}
-        {(isSelected) && (
-          <Billboard
-            position={buttonPositions[1]}>
-            <Html>
-              <Button variant='contained' size="small">0,1</Button>
-            </Html>
-          </Billboard>)}
-        {(isSelected) && (
-          <Billboard
-            position={buttonPositions[2]}>
-            <Html>
-              <Button variant='contained' size="small">-1,0</Button>
-            </Html>
-          </Billboard>)}
-        {(isSelected) && (
-          <Billboard
-            position={buttonPositions[3]}>
-            <Html>
-              <Button variant='contained' size="small">0,-1</Button>
-            </Html>
-          </Billboard>)}
-      </group>
-      <group
-        position={[x, yWithBase, z]}
         rotation={[0, (rotation * -Math.PI) / 3, 0]}
-        onPointerEnter={e => onPointerEnter(e, boardHex)}
-        onPointerOut={e => onPointerOut(e, boardHex)}
-        onPointerUp={e => onPointerUpLaurWall(e, boardHex)}
+        onPointerEnter={e => e.stopPropagation()}
+        onPointerOut={e => e.stopPropagation()}
+      // onPointerEnter={e => onPointerEnter(e, boardHex)}
+      // onPointerOut={e => e.stopPropagation()}
+      // onPointerOut={e => onPointerOut(e, boardHex)}
+      // onPointerUp={e => onPointerUpLaurWall(e, boardHex)}
       >
         <mesh
           geometry={nodes.PillarTop.geometry}
         // onPointerUp={e => onPointerUp(e, boardHex)}
         >
-          <meshMatcapMaterial color={isHighlighted ? yellowColor : pillarColor} />
+          <meshMatcapMaterial color={pillarColor} />
         </mesh>
         <mesh
           geometry={nodes.SubDecorCore.geometry}
         // onPointerUp={e => onPointerUp(e, boardHex)}
         >
           <meshMatcapMaterial
-            color={isHighlighted ? yellowColor : interiorPillarColor}
+            color={interiorPillarColor}
           />
         </mesh>
         <mesh
@@ -186,7 +151,7 @@ export default function LaurWallPillar({
         >
           <meshMatcapMaterial
             side={DoubleSide}
-            color={isHighlighted ? yellowColor : pillarColor}
+            color={pillarColor}
           />
         </mesh>
         <mesh
@@ -195,7 +160,7 @@ export default function LaurWallPillar({
         >
           <meshMatcapMaterial
             side={DoubleSide}
-            color={isHighlighted ? yellowColor : interiorPillarColor}
+            color={interiorPillarColor}
           />
         </mesh>
       </group>
