97cb6dcd2c3d171cbad7433cab8043108ad18436
John M <entity.john@gmail.com>
2025-01-05 17:29:49 -0600

    cleanup ticalla brush model in blender

--- src/world/models/TicallaBrush.tsx
+++ src/world/models/TicallaBrush.tsx
@@ -1,23 +1,17 @@
 import { useGLTF } from '@react-three/drei'
 import { BoardHex, HexTerrain } from '../../types'
 import { getBoardHex3DCoords } from '../../utils/map-utils'
-import { HEXGRID_HEXCAP_HEIGHT } from '../../utils/constants'
 import ObstacleBase from './ObstacleBase'
 import { hexTerrainColor } from '../maphex/hexColors'
 
 export default function TicallaBrush({ boardHex }: { boardHex: BoardHex }) {
-  const { x, y, z } = getBoardHex3DCoords(boardHex)
-  const yTree = y + HEXGRID_HEXCAP_HEIGHT / 2
-  const yBase = y + HEXGRID_HEXCAP_HEIGHT / 2
-  const {
-    nodes,
-    // materials
-  } = useGLTF('/ticalla-brush-colored-lowpoly.glb') as any
+  const { x, z, yBase, yWithBase } = getBoardHex3DCoords(boardHex)
+  const { nodes } = useGLTF('/ticalla-brush-colored-lowpoly.glb') as any
   const rotation = boardHex?.pieceRotation ?? 0
   return (
     <group>
       <group
-        position={[x + 0.35, yTree, z - 0.2]} // I botched this positioning in Blender
+        position={[x, yWithBase, z]}
         rotation={[0, (rotation * -Math.PI) / 3, 0]}
       >
         <mesh geometry={nodes.PlankFern1_1.geometry}>
