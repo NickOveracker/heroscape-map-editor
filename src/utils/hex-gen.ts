81b4b3e7ea5a2b9f44b190af0a41d81fd0fcae33
John M <entity.john@gmail.com>
2025-01-18 12:01:30 -0600

    remove shift of hexagon maps the old way, making new way

--- src/utils/hex-gen.ts
+++ src/utils/hex-gen.ts
@@ -1,48 +1,12 @@
-import { BoardHex, BoardHexes, CubeCoordinate, HexTerrain } from '../types'
+import { BoardHexes, CubeCoordinate, HexTerrain } from '../types'
 import { hexUtilsGenHexagonGrid, hexUtilsGenRectangleGrid } from './hex-utils'
 import { genBoardHexID } from './map-utils'
 
 export const generateHexagon = (mapSize: number): BoardHexes => {
-  // keeps our hex grid within same quadrant as rectangle map
   const boardHexes = hexesToEmptyBoardHexes(
-    translateHexagonHexesToNormal(hexUtilsGenHexagonGrid(mapSize), mapSize),
+    hexUtilsGenHexagonGrid(mapSize)
   )
-  // const boardHexes = hexesToEmptyBoardHexes(
-  //   hexUtilsGenHexagonGrid(mapSize)
-  // )
   return boardHexes
-}
-
-const qAdjust = 2 // why does dividing by 2 work?
-const translateHexagonHexesToNormal = (
-  hexes: CubeCoordinate[],
-  mapSize: number,
-): CubeCoordinate[] => {
-  return hexes.map((hex: CubeCoordinate) => {
-    return {
-      ...hex,
-      q: hex.q + mapSize / qAdjust,
-      r: hex.r + mapSize,
-    }
-  })
-}
-const translateHexagonBoardHexesToNormal = (
-  boardhexes: BoardHexes,
-  mapSize: number,
-): BoardHexes => {
-  const hexArray = Object.values(boardhexes)
-  return hexArray.reduce((prev: BoardHexes, curr: BoardHex) => {
-    const q = curr.q + mapSize / qAdjust
-    const r = curr.r + mapSize
-    const newID = genBoardHexID(curr)
-    prev[newID] = {
-      ...curr,
-      id: newID,
-      q,
-      r,
-    }
-    return prev
-  }, {} as BoardHexes)
 }
 
 export const generateRectangle = (
