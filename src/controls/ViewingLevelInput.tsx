e003538c57d5b11a7f8065dc7c6038e2a72ccb6f
John M <entity.john@gmail.com>
2025-01-30 03:11:16 -0600

    Revert "WIP: broken viewing level input"
    This reverts commit 13de9af9c073974a13f1ba857b6bc3db6ef5abdc.

--- src/controls/ViewingLevelInput.tsx
+++ src/controls/ViewingLevelInput.tsx
@@ -1,8 +1,7 @@
 import { Box, Grid2, Input, Typography } from '@mui/material'
 import useBoundStore from '../store/store'
-import { decodePieceID, getBoardPiecesMaxLevel } from '../utils/map-utils'
-import React, { ChangeEvent, useCallback, useMemo } from 'react'
-import { BoardPieces, Pieces } from '../types'
+import { getBoardPiecesMaxLevel } from '../utils/map-utils'
+import React from 'react'
 
 
 export default function ViewingLevelInput() {
@@ -10,53 +9,13 @@ export default function ViewingLevelInput() {
   const toggleViewingLevel = useBoundStore(s => s.toggleViewingLevel)
   const boardPieces = useBoundStore(s => s.boardPieces)
   const maxLevel = getBoardPiecesMaxLevel(boardPieces)
-
-  const filterBoardPieces = useCallback((level: number) => (
-    Object.keys(boardPieces).reduce((prev: BoardPieces, pid: string) => {
-      if (decodePieceID(pid).altitude < level) {
-        return { ...prev, [pid]: decodePieceID(pid).pieceID as Pieces }
-      } else {
-        return prev
-      }
-    }, {} as BoardPieces)
-  ), [boardPieces])
-
-  const filteredBoardPiecesDown = useMemo(() => (
-    filterBoardPieces(viewingLevel - 1)
-  ), [filterBoardPieces, viewingLevel])
-
-  const filteredBoardPiecesUp = useMemo(() => {
-    const newMax = getBoardPiecesMaxLevel(filterBoardPieces(viewingLevel + 1))
-    return newMax > viewingLevel ? filterBoardPieces(newMax) : filterBoardPieces(viewingLevel + 1)
-  }, [filterBoardPieces, viewingLevel])
-
-  const nextLevelDownWithPieces = getBoardPiecesMaxLevel(filteredBoardPiecesDown)
-  const nextLevelUpWithPieces = getBoardPiecesMaxLevel(filteredBoardPiecesUp)
-
   React.useEffect(() => {
     toggleViewingLevel(maxLevel)
   }, [boardPieces, toggleViewingLevel, maxLevel])
 
-  const handleChange = (event: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
-    const input = parseInt(event.target.value)
-    console.log("🚀 ~ handleChange ~ input:", input)
-    const isGoingUp = input > viewingLevel
-    const isGoingWayUp = isGoingUp && (input !== nextLevelUpWithPieces)
-    console.log("🚀 ~ handleChange ~ nextLevelUpWithPieces:", nextLevelUpWithPieces)
-    const isGoingDown = input < viewingLevel
-    const isGoingWayDown = isGoingDown && (input !== nextLevelDownWithPieces)
-    console.log("🚀 ~ handleChange ~ nextLevelDownWithPieces:", nextLevelDownWithPieces)
-    if (isGoingUp) {
-      toggleViewingLevel(isGoingWayUp ? nextLevelUpWithPieces : input)
-    }
-    else if (isGoingDown) {
-      toggleViewingLevel(isGoingWayDown ? nextLevelDownWithPieces - 1 : input)
-    } else {
-      toggleViewingLevel(input)
-    }
-  }
   return (
     <Box sx={{ width: 250 }}>
+
       <Grid2 container spacing={2} sx={{ alignItems: 'center' }}>
         <Grid2
           size={{ xs: 6 }}
@@ -71,7 +30,7 @@ export default function ViewingLevelInput() {
             value={viewingLevel}
             size="small"
             // disabled // TODO: implement viewing levels
-            onChange={handleChange}
+            onChange={(event) => toggleViewingLevel(parseInt(event.target.value))}
             inputProps={{
               step: 1,
               min: 0,
