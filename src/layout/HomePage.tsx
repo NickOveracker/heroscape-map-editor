72b9c6b0bd7e143c8aba49162d623cd839abc570
John M <entity.john@gmail.com>
2025-01-31 18:55:33 -0600

    move relative/floating things outisde layout div, for clarity

--- src/layout/HomePage.tsx
+++ src/layout/HomePage.tsx
@@ -12,58 +12,72 @@ import CameraSpeedDial from './CameraSpeedDial'
 export default function HomePage() {
   const cameraControlsRef = React.useRef(undefined!)
   // https://robohash.org/you.png?size=200x200
+
+  // MUI BREAKPOINTS
+  //   xs, extra-small: 0px
+  // sm, small: 600px
+  // md, medium: 900px
+  // lg, large: 1200px
+  // xl, extra-large: 1536px
+
+  // const isScreenOver1200 = useMediaQuery('(min-width:1200px)');
+  // const isScreenUnder900px = useMediaQuery('(max-width:900px)');
+
   const [isNavOpen, setIsNavOpen] = React.useState(false)
   const toggleIsNavOpen = (s: boolean) => {
     setIsNavOpen(s)
   }
   return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        width: '100%',
-        height: '100vh',
-        padding: 0,
-        margin: 0,
-        backgroundColor: 'var(--outer-space)',
-        justifyContent: 'space-between',
-      }}
-    >
+    <>
       <CreateMapFormDialog />
       <EditMapFormDialog />
-      <HeaderNav isNavOpen={isNavOpen} toggleIsNavOpen={toggleIsNavOpen} />
       <Drawer
         open={isNavOpen}
         // open={true} // DEV toggle
         onClose={() => toggleIsNavOpen(false)}
+        keepMounted
       >
         <DrawerList toggleIsNavOpen={toggleIsNavOpen} />
       </Drawer>
       <div
         style={{
-          // flex: 1,
-          position: 'relative',
-          width: '100%',
-          height: '70vh',
-        }}
-      >
-        <CameraSpeedDial cameraControlsRef={cameraControlsRef} />
-        <World cameraControlsRef={cameraControlsRef} />
-      </div>
-      <div
-        style={{
-          flex: 1,
           display: 'flex',
-          flexFlow: 'column nowrap',
+          flexDirection: 'column',
           width: '100%',
-          height: '30vh',
-          background: 'var(--black)',
-          overflow: 'auto',
+          height: '100vh',
+          padding: 0,
+          margin: 0,
+          backgroundColor: 'var(--outer-space)',
+          justifyContent: 'space-between',
         }}
       >
-        <Controls />
-        <LoadMapInputs />
+        <HeaderNav isNavOpen={isNavOpen} toggleIsNavOpen={toggleIsNavOpen} />
+        <div
+          style={{
+            // flex: 1,
+            position: 'relative',
+            width: '100%',
+            height: '70vh',
+          }}
+        >
+          <CameraSpeedDial cameraControlsRef={cameraControlsRef} />
+          <World cameraControlsRef={cameraControlsRef} />
+        </div>
+        <div
+          style={{
+            flex: 1,
+            display: 'flex',
+            flexFlow: 'column nowrap',
+            width: '100%',
+            height: '30vh',
+            background: 'var(--black)',
+            overflow: 'auto',
+          }}
+        >
+          <Controls />
+          <LoadMapInputs />
+        </div>
       </div>
-    </div>
+    </>
   )
 }
