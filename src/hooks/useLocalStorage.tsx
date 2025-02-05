d988076c8c8205729f4152694fe1219c2c8c6027
John M <entity.john@gmail.com>
2025-02-05 02:52:08 -0600

    add fn to estimate local storage space, need LS system

--- src/hooks/useLocalStorage.tsx
+++ src/hooks/useLocalStorage.tsx
@@ -1,5 +1,21 @@
 import { useEffect, useState } from 'react'
 
+// https://stackoverflow.com/questions/4391575/how-to-find-the-size-of-localstorage
+// GETS MEMORY OF LOCAL STORAGE
+
+// var _lsTotal = 0,
+//     _xLen, _x;
+// for (_x in localStorage) {
+//     if (!localStorage.hasOwnProperty(_x)) {
+//         continue;
+//     }
+//     _xLen = ((localStorage[_x].length + _x.length) * 2);
+//     _lsTotal += _xLen;
+//     console.log(_x.substr(0, 50) + " = " + (_xLen / 1024).toFixed(2) + " KB")
+// };
+// console.log("Total = " + (_lsTotal / 1024).toFixed(2) + " KB");
+
+
 export function useLocalStorage<T>(
   key: string,
   initialValue: T,
